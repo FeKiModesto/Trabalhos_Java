e337948149712f4e095a63d518175eaa2ea11d5d
FeKiModesto <fekimodesto04@gmail.com>
2026-05-07 09:38:54 -0300

    Remove pasta duplicada cervejaria/cervejaria

--- cervejaria/cervejaria/src/main/java/fiap/com/br/cervejaria/repository/CervejariaRepository.java
+++
@@ -1,7 +0,0 @@
-package fiap.com.br.brewery.repository;
-
-import fiap.com.br.brewery.entity.Cervejaria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CervejariaRepository extends JpaRepository<Cervejaria, Long> {
-}
--- cervejaria/cervejaria/src/main/java/fiap/com/br/cervejaria/repository/EstiloRepository.java
+++
@@ -1,7 +0,0 @@
-package fiap.com.br.brewery.repository;
-
-import fiap.com.br.brewery.entity.Estilo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstiloRepository extends JpaRepository<Estilo, Long> {
-}
