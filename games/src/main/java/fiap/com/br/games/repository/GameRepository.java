6841270be40e92bdeeabb845d04c5cffb08921a7
FeKiModesto <fekimodesto04@gmail.com>
2026-05-12 15:52:16 -0300

    Adiciona GameRepository com queries customizadas

---
+++ games/src/main/java/fiap/com/br/games/repository/GameRepository.java
@@ -0,0 +1,12 @@
+package fiap.com.br.games.repository;
+
+import fiap.com.br.games.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+    List<Game> findByGenre(String genre);
+    List<Game> findByPlatform(String platform);
+    List<Game> findByDeveloper(String developer);
+}
