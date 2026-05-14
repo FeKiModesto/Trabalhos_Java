65e80624e40c9ef651670a0ba292fe9830a89e9b
FeKiModesto <fekimodesto04@gmail.com>
2026-05-14 08:23:36 -0300

    Atualiza GameRepository com queries por genreId e platformId

--- games/src/main/java/fiap/com/br/games/repository/GameRepository.java
+++ games/src/main/java/fiap/com/br/games/repository/GameRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
-    List<Game> findByGenre(String genre);
-    List<Game> findByPlatform(String platform);
-    List<Game> findByDeveloper(String developer);
+    List<Game> findByGenreId(Long genreId);
+    List<Game> findByPlatformId(Long platformId);
 }
