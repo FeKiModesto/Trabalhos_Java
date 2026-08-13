76a67c45fa92295165012b6a42661ac1533c8577
FeKiModesto <fekimodesto04@gmail.com>
2026-08-13 08:50:11 -0300

    chore: criação de pastas de separação por semestre.

---
+++ 2º Semestre/mypass-projeto/mypass/src/test/java/fiap/com/br/mypass/MyPassApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.com.br.mypass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
