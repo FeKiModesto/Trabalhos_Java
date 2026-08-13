b7071ae6273820c0044eb7a6edf0e3d8f8e3e5ce
Modesto Felipe <fekimodesto04@gmail.com>
2026-08-13 08:53:49 -0300

    refactor: removendo diretório com estrutura incorreta
    - A reorganização dos arquivos e códigos será feita em um commit subsequente.

--- 2º Semestre/mypass-projeto/mypass/src/main/java/fiap/com/br/mypass/password/PasswordRepository.java
+++
@@ -1,6 +0,0 @@
-package fiap.com.br.mypass.password;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PasswordRepository extends JpaRepository<Password, Long> {
-}
