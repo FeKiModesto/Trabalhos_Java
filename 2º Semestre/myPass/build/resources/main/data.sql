65027809ead1da71744d874e77952508e5936bfb
FeKiModesto <fekimodesto04@gmail.com>
2026-08-21 02:16:07 -0300

    chore: remoção de arquivos gerados do projeto myPass (.gradle, .idea, build)

--- 2º Semestre/myPass/build/resources/main/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO passwords (url, username, password) VALUES ('https://example.com', 'johndoe', 'password123');
-INSERT INTO passwords (url, username, password) VALUES ('https://mail.example.com', 'janedoe', 'securePass!');
-INSERT INTO passwords (url, username, password) VALUES ('https://banking.example.com', 'mike123', 'bank$ecure987');
-INSERT INTO passwords (url, username, password) VALUES ('https://social.example.com', 'emily', 'Pa$$word!1');
-INSERT INTO passwords (url, username, password) VALUES ('https://store.example.com', 'alice_wonder', 'alice$h0pp1ng');
